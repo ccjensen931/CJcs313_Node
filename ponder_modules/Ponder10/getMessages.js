3dbc5308e08f229839b031b00f5749a90ae7b733
ccjensen931 <ccjensen1179@gmail.com>
2019-06-28 18:14:44 -0600

    Finishing ponder10

---
+++ ponder_modules/Ponder10/getMessages.js
@@ -0,0 +1,44 @@
+const pool = require('../../general_modules/dbConnect.js');
+
+module.exports = {
+    getInbox: function(req, res) {
+        const query = {
+            name: 'get-inbox',
+            text: 'SELECT m.message_id, m.subject_text, u.username, m.message_text, m.message_read FROM messages m JOIN message_received r ON m.message_id = r.message_id JOIN message_sent s ON m.message_id = s.message_id JOIN users u on s.sender_id = u.user_id WHERE r.recipient_id = 1;',
+            //values: [user_id]
+        };
+
+        pool.query(query, function(err, result) {
+            // If an error occurred...
+            if (err) {
+                console.log("Error in query: ")
+                console.log(err);
+        }   
+
+            // Log this to the console for debugging purposes.
+            console.log("Back from DB with result:");
+            console.log(result.rows);
+
+            res.json({messages: result.rows});
+    });},
+    getMessage: function(req, res) {
+        const query = {
+            name: 'get-message',
+            text: 'SELECT message_text FROM messages WHERE message_id = $1',
+            values: [req.query.id]
+        };
+
+        pool.query(query, function(err, result) {
+            // If an error occurred...
+            if (err) {
+                console.log("Error in query: ")
+                console.log(err);
+            }
+    
+            // Log this to the console for debugging purposes.
+            console.log("Back from DB with result:");
+            console.log(result.rows);
+    
+            res.json({message: result.rows});
+        });}
+};
--- ponder_modules/Ponder10/route.js
+++ ponder_modules/Ponder10/route.js
@@ -2,11 +2,12 @@ const express = require('express');
 let parser = require('body-parser');
 let router = express.Router();
 
-let getInbox = require('./getInbox.js');
+let getMessages = require('./getMessages.js');
 
 router.use(parser.urlencoded({extended : true}));
 router.get('/ponder10/', (req, res) => res.send('Navigate to ponder10/inbox'));
 router.get('/ponder10/inbox', (req, res) => res.render('Ponder10/messageInbox'));
-router.get('/ponder10/getInbox', (req, res) => { let messages = getInbox(req, res); res.json({"messages: " : messages}) });
+router.get('/ponder10/getInbox', (req, res) => getMessages.getInbox(req, res));
+router.get('/ponder10/getMessage', (req, res) => getMessages.getMessage(req, res));
 
 module.exports = router;
